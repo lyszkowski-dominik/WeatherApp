95900edc1feeca99803ae8bb3a2d03fc1166667d
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2021-09-02 20:37:08 +0200

    Weather App Stage 3 Working

--- app.py
+++ app.py
@@ -12,18 +12,15 @@ def index():
     return render_template('index.html')
 # don't change the following way to run flask:
 
+
 @app.route('/add', methods=['POST'])
 def add_city():
     if request.method == 'POST':
         request_city = request.form['city_name']
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=6476e9515765d4437dc65184cdf34309&units=metric&lang=pl'.format(request_city)
         r = requests.get(url).json()
-        print(r)
-        req_weather = {'name': r['name'] , 'temp': round(r['main']['temp'],1), 'state': r['weather'][0]['description']}
+        req_weather = {'name': r['name'], 'temp': round(r['main']['temp'], 1), 'state': r['weather'][0]['description']}
     return render_template('index.html', weather=req_weather)
-
-
-
 
 
 if __name__ == '__main__':
